b0519684d2e1c09ad486ad55b0443c1865760087
reynis <primo.animus@gmail.com>
2019-07-18 20:05:26 +0545

    MainActivity VCS xml file commited.

--- app/src/main/java/com/example/gitinandroid/MainActivity.java
+++ app/src/main/java/com/example/gitinandroid/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("Mytag","Hello World!!");
+        Log.i("Mytag","Hello World!!");
+
     }
 }
